06f1c1440ae3a988cd95ff769e5fdfcb6f8816bd
Leonel Meque <leomeque@gmail.com>
2023-12-07 13:04:58 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Leonel Tomás Alexandre Meque on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
