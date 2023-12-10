f324330094aa1367ca24bcb4a8eff69757156fae
Leonel Meque <leomeque@gmail.com>
2023-12-11 00:39:41 +0100

    managing-state-and-life-cycle

---
+++ Scrumdinger/Views/MeetingView.swift
@@ -0,0 +1,39 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
+
+import SwiftUI
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+                Circle()
+                    .strokeBorder(lineWidth: 24) 
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
+                }
+            }
+            .padding()
+            .foregroundColor(scrum.theme.accentColor)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
+}
